4d26eb0e5b889adcf38023cffffe3a5d2f6b1566
Norman-715 <65022312+Norman-715@users.noreply.github.com>
2020-06-19 01:32:56 +0200

    alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,8 +12,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**`Hey! I'm alive. All systems online and functioning normally... ψ(｀∇´)ψ`**\n\n"
-                      "` 🔸 Telethon version:` **6.9.0**\n` 🔹 Python:` **3.7.3** \n` 🔸 by:` @mredition\n"
-                     "` 🔹 Bot created by:` [MR-EDITION](tg://user?id=794996256)\n"
+                      "` 🔸 Telethon version:` **6.9.0**\n` 🔹 Python:` **3.7.3** \n` 🔸 by:` @KidLuk\n"
+                     "` 🔹 Bot created by:` [LUKHELE](tg://user?id=794996256)\n"
                      "` 🔸 Database Status:` **All OK 👌!**\n"
                      f"` 🔹 My peru owner`: {DEFAULTUSER}\n\n"
                      "           [Deploy this userbot Now](https://github.com/mredition/raynaldbot)")
