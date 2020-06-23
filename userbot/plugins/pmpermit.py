14e86c1756cc23ae3f72d07a9b8ae9d01825c14a
Norman-715 <65022312+Norman-715@users.noreply.github.com>
2020-06-23 16:43:21 +0200

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -13,7 +13,7 @@ PREV_REPLY_MESSAGE = {}
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No name set yet nibba, check pinned message in** @raynaldbot"
 USER_BOT_WARN_ZERO = "`You were spamming my master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` **Now GTFO, i'm playing minecraft** "
 USER_BOT_NO_WARN = ("[...........███ ]▄▄▄▄▄▃ \n..▂▄▅█████▅▄▃▂\n[███████████████]\n◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤](tg://user?id=794996256)\n\n"
-                    "`Hello, this is MR EDITION BOT Security Service. This is an automated message in `"
+                    "`Hello, this is LUKHELE'S Security Service. This is an automated message in `"
                     f"{DEFAULTUSER}'s` inbox.\n\n"
                     "Do not spam here, else you will be blocked automatically.`\n\n"
                     "** Send** `/start` ** to access the available options.**")
